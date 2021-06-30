88d85f585e2193409783625670d1c80e48ec9984
sunbaoxu <1194063234@qq.com>
2021-06-30 16:23:31 +0800

    初始化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home-wrap">
+    这是audio的demo页面
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
+<style lang="scss" scoped>
+.home-wrap{
+  
+}
+</style>
